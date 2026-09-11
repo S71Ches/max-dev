2644404cb9d6600657e3826b25af8293c41cf11e
S71Ches <masyacipa124@gmail.com>
2026-09-11 14:18:55 +0300

    Configure Vite multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/max-dev/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        contacts: resolve(__dirname, 'contacts.html'),
+      },
+    },
+  },
 })
